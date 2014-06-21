d5682822d1bb58dd1a45872b4069158a6e4193f2
John Urbank <jurbank@gmail.com>
2014-06-20 23:25:09 -0400

    navigation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,3 +4,28 @@
 // 	$(window).
 // };
 
+// var canvas = document.getElementById('canvas'),
+//   ctx = canvas.getContext('2d'),
+//   W = window.innerWidth,
+//   H = window.innerHeight;
+
+// canvas.width = W;
+// canvas.height = H;
+
+//helps prevent skewing and canvas resizing
+// window.onresize = function(event) {
+//   W = window.innerWidth,
+//   H = window.innerHeight;
+//   canvas.width = W;
+//   canvas.height = H;
+// }
+
+$(document).ready(function() {
+    var hamburger = $('#hamburger-icon');
+    hamburger.click(function() {
+        hamburger.toggleClass('active');
+        return false;
+    });
+});
+
+
